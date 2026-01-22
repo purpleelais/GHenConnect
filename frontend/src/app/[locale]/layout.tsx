8419f7ee3cb781c4356e99864f6bc67c285b2695
Nicolas Pereira <nicolasp.reis7@gmail.com>
2026-01-21 23:11:08 -0300

    feat: initialize frontend with Next.js, Tailwind CSS, and TypeScript

---
+++ frontend/src/app/[locale]/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "@/presentation/styles/globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "GHenConnect",
+  description: "Developed by pp-lab",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
